fb705c03dda366692d94c0daf20d3c49b9c601b3
asilbekkurbashov <kurbashovasilbek02@gmail.com>
2023-12-13 22:31:17 +0500

    pizza

--- src/App/components/sort/Sort.tsx
+++ src/App/components/sort/Sort.tsx
@@ -1,41 +1,41 @@
-import { useAppDispatch, useAppSelector } from '@/hooks/useRedux';
-import { useResponsive } from '@/hooks/useResponsive';
-import { sort } from '@/shared/data/data'
-import { SortActions } from '@/store/sort/SortSlice';
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { useResponsive } from "@/hooks/useResponsive";
+import { sort } from "@/shared/data/data";
+import { SortActions } from "@/store/sort/SortSlice";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 function Sort() {
-    const { sortValue } = useAppSelector((state) => state.SortReducer);
-    const {isMobile} = useResponsive(600)
-    const dispatch = useAppDispatch()
-    const handleSort = (value: string) => {
-        dispatch(SortActions.setSort(value));
-      };
+  const { sortValue } = useAppSelector((state) => state.SortReducer);
+  const { isMobile } = useResponsive(600);
+  const dispatch = useAppDispatch();
+  const handleSort = (value: string) => {
+    dispatch(SortActions.setSort(value));
+  };
   return (
     <Box width={200}>
-          <FormControl fullWidth variant="standard" color="warning">
-            <InputLabel id="demo-simple-select-label">Сортировка по</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Сортировка по"
-              value={sortValue}
-              size={'small'}
-              style={{fontSize:isMobile ? '12px' : ''}}
+      <FormControl fullWidth variant="standard" color="warning">
+        <InputLabel id="demo-simple-select-label">Сортировка по</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Сортировка по"
+          value={sortValue}
+          size={"small"}
+          style={{ fontSize: isMobile ? "12px" : "" }}
+        >
+          {sort.map((elem) => (
+            <MenuItem
+              onClick={() => handleSort(elem.name)}
+              key={elem.name}
+              value={elem.name}
             >
-              {sort.map((elem) => (
-                <MenuItem
-                  onClick={() => handleSort(elem.name)}
-                  key={elem.name}
-                  value={elem.name}
-                >
-                  {elem.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-  )
+              {elem.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
 }
 
-export default Sort
+export default Sort;
