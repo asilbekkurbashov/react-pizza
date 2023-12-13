23854b52d7fa51227b08a28d76dad31c8a6dfcf3
asilbekkurbashov <kurbashovasilbek02@gmail.com>
2023-12-13 22:40:25 +0500

    pizza

--- src/shared/ui/createTheme/index.ts
+++ src/shared/ui/createTheme/index.ts
@@ -1,23 +1,19 @@
 import { createTheme } from '@mui/material/styles';
-// createColor __ start =================
 declare module '@mui/material/styles' {
   interface Palette {
     neutral: Palette['primary'];
   }
 
-  // allow configuration using `createTheme`
   interface PaletteOptions {
     neutral?: PaletteOptions['primary'];
   }
 }
 
-// Update the Button's color prop options
 declare module '@mui/material/Button' {
   interface ButtonPropsColorOverrides {
     neutral: true;
   }
 }
-// createColor __ end ===================
 export const dark = createTheme({
   palette: {
     neutral: {
